efa5e81d19ecf48aa10dcc5afdd4be2592e082cb
Aarzavjain <140225555+Aarzavjain@users.noreply.github.com>
2023-07-22 10:32:21 +0530

    Update RPS.py

--- Hydroponic1.py
+++ Hydroponic1.py
@@ -1,62 +1,43 @@
-import time
-import Adafruit_DHT
-import RPi.GPIO as GPIO
 
-# Sensor and Actuator Configuration
-DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 17  # GPIO pin where DHT11 data pin is connected
-WATER_LEVEL_PIN = 18  # GPIO pin where water level sensor is connected
-WATER_PUMP_PIN = 27  # GPIO pin where water pump relay is connected
+import random
 
-# Threshold for turning on the water pump (example value)
-WATER_LEVEL_THRESHOLD = 30  # Adjust this value based on your setup
+def get_user_choice():
+    user_choice = input("Enter your choice (Rock, Paper, or Scissors): ").lower()
+    while user_choice not in ["rock", "paper", "scissors"]:
+        print("Invalid choice. Please choose Rock, Paper, or Scissors.")
+        user_choice = input("Enter your choice (Rock, Paper, or Scissors): ").lower()
+    return user_choice
 
-# Initialize GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(WATER_LEVEL_PIN, GPIO.IN)
-GPIO.setup(WATER_PUMP_PIN, GPIO.OUT)
+def get_computer_choice():
+    return random.choice(["rock", "paper", "scissors"])
 
-def read_dht_sensor():
-    # Read temperature and humidity from DHT11 sensor
-    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        return round(temperature, 2), round(humidity, 2)
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+         (user_choice == "paper" and computer_choice == "rock") or \
+         (user_choice == "scissors" and computer_choice == "paper"):
+        return "Congratulations! You win!"
     else:
-        return None, None
+        return "Sorry, you lose. Computer wins!"
 
-def read_water_level():
-    # Read water level sensor data (example: 0 - water present, 1 - water absent)
-    return GPIO.input(WATER_LEVEL_PIN)
+def play_again():
+    return input("Do you want to play again? (yes or no): ").lower() == "yes"
 
-def control_water_pump(status):
-    # Control water pump based on the status (True - ON, False - OFF)
-    GPIO.output(WATER_PUMP_PIN, status)
+def main():
+    print("Welcome to Rock, Paper, Scissors!")
+    play_game = True
 
-try:
-    while True:
-        # Read sensor data
-        temperature, humidity = read_dht_sensor()
-        water_level = read_water_level()
+    while play_game:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        print(f"You chose: {user_choice}")
+        print(f"Computer chose: {computer_choice}")
 
-        # Print sensor data
-        if temperature is not None and humidity is not None:
-            print(f"Temperature: {temperature} °C, Humidity: {humidity}%")
-        else:
-            print("Failed to retrieve DHT11 sensor data.")
+        result = determine_winner(user_choice, computer_choice)
+        print(result)
 
-        # Control water pump based on water level
-        if water_level == 0:
-            print("Water level is normal.")
-            control_water_pump(False)  # Turn off the water pump
-        else:
-            print("Water level is low. Turning on the water pump.")
-            control_water_pump(True)   # Turn on the water pump
+        play_game = play_again()
 
-        print("------------------------")
-        time.sleep(5)  # 5 seconds delay between readings
-
-except KeyboardInterrupt:
-    print("Exiting...")
-finally:
-    # Cleanup GPIO configuration
-    GPIO.cleanup()
+if __name__ == "__main__":
+    main()
